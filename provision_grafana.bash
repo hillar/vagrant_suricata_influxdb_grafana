4d32bc3b4cf33dc800db62f8fbc7d93b59b48284
Hillar <hillar@aarelaid.net>
2014-11-15 12:26:14 +0200

    added proxy for griffin

--- provision_grafana.bash
+++ provision_grafana.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+mkdir -p /opt/grafana
+cd /opt/grafana
+
+git clone https://github.com/strima/grafana-authentication-proxy.git
 wget http://grafanarel.s3.amazonaws.com/grafana-1.8.1.tar.gz
+
+
--- provision_influxdb.bash
+++ provision_influxdb.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
+wget -q http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 sudo dpkg -i influxdb_latest_amd64.deb
 service influxdb start
-sleep 1
-curl -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "provision_test"}'
-curl 'http://localhost:8086/db?u=root&p=root'
-curl -X DELETE 'http://localhost:8086/db/provision_test?u=root&p=root'
-curl 'http://localhost:8086/db?u=root&p=root'
+sleep 5
+curl -s -XPOST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "provision_test"}'
+curl -s 'http://localhost:8086/db?u=root&p=root'
+curl -s -X DELETE 'http://localhost:8086/db/provision_test?u=root&p=root'
+curl -s 'http://localhost:8086/db?u=root&p=root'
