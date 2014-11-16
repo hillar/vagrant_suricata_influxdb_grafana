6b1f338bcfec20c0567b89a5c157f98c4f20fc2e
Hillar <hillar@aarelaid.net>
2014-11-16 20:13:48 +0200

    added grafana config

--- provision_grafana.bash
+++ provision_grafana.bash
@@ -4,12 +4,12 @@ apt-get -y -qq install golang-go mercurial
 
 echo "getting grafana-1.8.1 ..."
 cd /tmp
-wget http://grafanarel.s3.amazonaws.com/grafana-1.8.1.tar.gz
+wget -q http://grafanarel.s3.amazonaws.com/grafana-1.8.1.tar.gz
 tar -xzf grafana-1.8.1.tar.gz
 mkdir -p /opt/grafana/www/
 mv grafana-1.8.1 /opt/grafana/www/
-cp /opt/grafana/www/grafana-1.8.1/config.sample.js /opt/grafana/www/grafana-1.8.1/config.js
-
+wget -q https://raw.githubusercontent.com/hillar/vagrant_suricata_influxdb_grafana/master/config.js.grafana
+mv config.js.grafana /opt/grafana/www/grafana-1.8.1/config.js
 
 
 echo "getting  & building grafana proxy ..."
@@ -21,6 +21,6 @@ cp /tmp/go/bin/grafanaweb /opt/grafana/bin/grafanaweb
 ldd /opt/grafana/bin/grafanaweb
 
 
-#/opt/grafana/bin/grafanaweb -proxy="http://192.168.33.111:8086" -root=/opt/grafana/www/grafana-1.8.1 &
+/opt/grafana/bin/grafanaweb -listen="192.168.33.112:3003" -proxy="http://192.168.33.111:8086" -root=/opt/grafana/www/grafana-1.8.1 &
 
-
+curl http://192.168.33.112:3003/
